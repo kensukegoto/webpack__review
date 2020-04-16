e52205e7891abd094bc40c873206c17292cf41b3
kensuke goto <kensuke.goto.1981@gmail.com>
2020-04-16 15:15:21 +0900

    implement url-loader and file-loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import './style.css';
+import man from './images/b-mv__man.jpg';
 
 function component(){
   const element = document.createElement("div");
@@ -10,3 +11,7 @@ function component(){
 
 document.body.appendChild(component());
 // document.body.classList.add('haikei');
+
+const image = new Image();
+image.src = man;
+document.body.appendChild(image);
